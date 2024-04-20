5dcb3c896992a35424d2ebbc06efa6de29f657bf
Bluend007 <lokeshrawatrawat123@gmail.com>
2024-04-20 23:42:05 +0530

    add more clog for find error3

--- scraper/md_web_scraper.js
+++ scraper/md_web_scraper.js
@@ -5,15 +5,17 @@ const md_web_scraper = async (name) => {
     console.log(`searching for ${name} on md inside try`);
 
     const url = `https://mdcomputers.in/index.php?search=${name}&submit_search=&route=product%2Fsearch`;
-    console.log(url)
+    console.log(url);
     console.log(`searching for ${name} on md inside try after url`);
 
     const browser = await puppeteer.launch({ headless: true });
+
+    console.log(`searching for ${name} on md inside try after browser`);
+
     const page = await browser.newPage();
     await page.goto(url);
 
-    const pageContent = await page.content();
-    console.log("Page Content:", pageContent);
+    console.log("Page Content:");
 
     // Check if the elements exist before accessing them
     const priceElement = await page.$(".price-new");
