27acdb52159132a543991a4d5f5d12eb64561874
Bluend007 <lokeshrawatrawat123@gmail.com>
2024-04-20 23:52:03 +0530

    test commit add from puppeter git

--- scraper/md_web_scraper.js
+++ scraper/md_web_scraper.js
@@ -2,50 +2,83 @@ import puppeteer from "puppeteer";
 const md_web_scraper = async (name) => {
   console.log(`searching for ${name} on md`);
   try {
-    console.log(`searching for ${name} on md inside try`);
 
-    const url = `https://mdcomputers.in/index.php?search=${name}&submit_search=&route=product%2Fsearch`;
-    console.log(url);
-    console.log(`searching for ${name} on md inside try after url`);
 
-    const browser = await puppeteer.launch({ headless: true });
 
-    console.log(`searching for ${name} on md inside try after browser`);
 
-    const page = await browser.newPage();
-    await page.goto(url);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    console.log("Page Content:");
+  // Navigate the page to a URL
+  await page.goto('https://developer.chrome.com/');
 
-    // Check if the elements exist before accessing them
-    const priceElement = await page.$(".price-new");
-    const titleElement = await page.$(".right-block h4 a");
-    const imageElement = await page.$(".product-image-container img");
-    const linkElement = await page.$(".product-image-container a");
+  // Set screen size
+  await page.setViewport({width: 1080, height: 1024});
 
-    // Extract data if elements exist
-    const allArticles = {};
-    if (priceElement) {
-      allArticles.source = "md computers";
-      allArticles.image = await page.evaluate(
-        (element) => element.src,
-        imageElement,
-      );
-      allArticles.link = await page.evaluate(
-        (element) => element.href,
-        linkElement,
-      );
-      allArticles.price = await page.evaluate(
-        (element) => element.innerText,
-        priceElement,
-      );
-      allArticles.title = await page.evaluate(
-        (element) => element.innerText.slice(0, 50),
-        titleElement,
-      );
-    }
-    await browser.close();
+  // Type into search box
+  await page.type('.devsite-search-field', 'automate beyond recorder');
 
+  // Wait and click on first result
+  const searchResultSelector = '.devsite-result-item-link';
+  await page.waitForSelector(searchResultSelector);
+  await page.click(searchResultSelector);
+
+  // Locate the full title with a unique string
+  const textSelector = await page.waitForSelector(
+    'text/Customize and automate'
+  );
+  const fullTitle = await textSelector?.evaluate(el => el.textContent);
+
+  // Print the full title
+  console.log('The title of this blog post is "%s".', fullTitle);
+
+  await browser.close();
+
+
+    // console.log(`searching for ${name} on md inside try`);
+    //
+    // const url = `https://mdcomputers.in/index.php?search=${name}&submit_search=&route=product%2Fsearch`;
+    // console.log(url);
+    // console.log(`searching for ${name} on md inside try after url`);
+    //
+    // const browser = await puppeteer.launch({ headless: true });
+    //
+    // console.log(`searching for ${name} on md inside try after browser`);
+    //
+    // const page = await browser.newPage();
+    // await page.goto(url);
+    //
+    // console.log("Page Content:");
+    //
+    // // Check if the elements exist before accessing them
+    // const priceElement = await page.$(".price-new");
+    // const titleElement = await page.$(".right-block h4 a");
+    // const imageElement = await page.$(".product-image-container img");
+    // const linkElement = await page.$(".product-image-container a");
+    //
+    // // Extract data if elements exist
+    // const allArticles = {};
+    // if (priceElement) {
+    //   allArticles.source = "md computers";
+    //   allArticles.image = await page.evaluate(
+    //     (element) => element.src,
+    //     imageElement,
+    //   );
+    //   allArticles.link = await page.evaluate(
+    //     (element) => element.href,
+    //     linkElement,
+    //   );
+    //   allArticles.price = await page.evaluate(
+    //     (element) => element.innerText,
+    //     priceElement,
+    //   );
+    //   allArticles.title = await page.evaluate(
+    //     (element) => element.innerText.slice(0, 50),
+    //     titleElement,
+    //   );
+    // }
+    // await browser.close();
+    //
     // Return the scraped data
     return allArticles;
   } catch (error) {
